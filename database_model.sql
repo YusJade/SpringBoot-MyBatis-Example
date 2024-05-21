2639bfe877a59eeabf45007292a08bf3668b2f8a
YuXeng <1917912744@qq.com>
2024-05-21 21:09:09 +0800

    初步完成数据库建模

---
+++ database_model.sql
@@ -0,0 +1,54 @@
+CREATE TABLE `tb_admin` (
+  `id` int PRIMARY KEY,
+  `username` varchar(50) UNIQUE,
+  `password` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `created_at` datetime DEFAULT (now())
+);
+
+CREATE TABLE `tb_book_category` (
+  `id` int PRIMARY KEY,
+  `name` varchar(50) UNIQUE,
+  `quantity` int,
+  `created_at` datetime DEFAULT (now())
+);
+
+CREATE TABLE `tb_book` (
+  `id` int PRIMARY KEY,
+  `title` varchar(50),
+  `author` varchar(50),
+  `category_id` int,
+  `publisher` varchar(50),
+  `quantity` int,
+  `created_at` datetime DEFAULT (now())
+);
+
+CREATE TABLE `tb_borrow` (
+  `id` int PRIMARY KEY,
+  `user_id` int,
+  `book_id` int,
+  `borrow_date` datetime DEFAULT (now()),
+  `return_date` datetime,
+  `really_return_date` datetime
+);
+
+CREATE TABLE `tb_user` (
+  `id` int PRIMARY KEY,
+  `username` varchar(50) UNIQUE,
+  `password` varchar(50) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `gender` varchar(50) DEFAULT '男',
+  `phone` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `max_borrow_days` int DEFAULT 30,
+  `max_borrow_books` int DEFAULT 5,
+  `created_at` datetime DEFAULT (now())
+);
+
+ALTER TABLE `tb_book` ADD FOREIGN KEY (`category_id`) REFERENCES `tb_book_category` (`id`);
+
+ALTER TABLE `tb_borrow` ADD FOREIGN KEY (`user_id`) REFERENCES `tb_user` (`id`);
+
+ALTER TABLE `tb_borrow` ADD FOREIGN KEY (`book_id`) REFERENCES `tb_book` (`id`);
