6b1cefc320c2d1ecee5732893a1ed1a376de0209
YuXeng <1917912744@qq.com>
2024-05-24 08:39:09 +0800

    规范 RESTful 接口

--- demo_display/src/main/java/com/yuxeng/display/usermodel/Email/AuthServiceDto.java
+++ demo_display/src/main/java/com/yuxeng/display/usermodel/Email/AuthServiceDto.java
@@ -5,8 +5,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+//@AllArgsConstructor
+//@NoArgsConstructor
 public class AuthServiceDto {
 
 }
