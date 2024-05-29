9159250fa934563931b85cafd175f5e32a9d40f9
YuXeng <1917912744@qq.com>
2024-05-29 15:24:36 +0800

    添加触发器

--- database_model.sql
+++ database_model.sql
@@ -65,7 +65,7 @@ BEGIN
     IF NEW.return_date IS NULL AND OLD.return_date IS NOT NULL THEN
         UPDATE tb_book SET quantity = quantity - 1 WHERE id = NEW.book_id;
     END IF;
-end;
+END;
 
 # 有借阅记录插入时，更新被借阅书籍的数目
 CREATE TRIGGER trg_tb_borrow_upd_book_quantity_i
@@ -74,7 +74,7 @@ BEGIN
     IF NEW.return_date IS NULL THEN
         UPDATE tb_book SET quantity = quantity - 1 WHERE id = NEW.book_id;
     END IF;
-end;
+END;
 
 # 当某一图书数量更新时，对应的图书类别中的图书总数需要更新
 CREATE TRIGGER trg_tb_book_upd_category_quantity_u
@@ -82,7 +82,7 @@ AFTER UPDATE on tb_book FOR EACH ROW
 BEGIN
     UPDATE tb_book_category SET quantity = quantity - OLD.quantity + NEW.quantity
     WHERE  tb_book_category.id = NEW.category_id;
-end;
+END;
 
 # 当有图书信息插入时，对应的图书类别中的图书总数需要更新
 CREATE TRIGGER trg_tb_book_upd_category_quantity_i
@@ -90,7 +90,7 @@ AFTER INSERT on tb_book FOR EACH ROW
 BEGIN
     UPDATE tb_book_category SET quantity = quantity + NEW.quantity
     WHERE  tb_book_category.id = NEW.category_id;
-end;
+END;
 
 # 当某一图书信息被删除时，对应的图书类别中的图书总数需要更新
 CREATE TRIGGER trg_tb_book_upd_category_quantity_d
@@ -98,4 +98,4 @@ AFTER DELETE on tb_book FOR EACH ROW
 BEGIN
     UPDATE tb_book_category SET quantity = quantity - OLD.quantity
     WHERE  tb_book_category.id = OLD.category_id;
-end;
+END;
